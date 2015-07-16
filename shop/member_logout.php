f76c49063494bbbbb7cbaaec9c1cdb3ff5bc8eb5
masashi tajima <real2003mt@yahoo.co.jp>
2015-07-17 01:39:38 +0900

    edit ecsample

---
+++ shop/member_logout.php
@@ -0,0 +1,25 @@
+<?php
+$_SESSION=array();
+if(isset($_COOKIE[session_name()])==true)
+{
+
+    setcookie(session_name(),'',time()-42000,'/');
+
+}
+@session_destroy();
+?>
+
+<!DOCTYPE HTML PUBLIC"-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>PHP基礎</title>
+</head>
+<body>
+
+ログアウトしました。<br/>
+<br/>
+<a href="shop_list.php">商品一覧へ</a>
+
+</body>
+</html>
